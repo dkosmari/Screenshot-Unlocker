e26a73babe09c3663bfa26398c442c3febaec9ab
Daniel K. O. (dkosmari) <none@none>
2024-08-08 23:31:31 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Screenshot Unlocker
+
+Some games and apps disable the upload of screenshots through the Home Menu's web
+browser. This plugin overrides the setting, to always enable screenshots.
+
+Note: this plugin has no relationship to the [official Screenshot
+plugin](https://github.com/wiiu-env/ScreenshotWUPS) from Aroma.
+
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+/*
+ * Screenshot Unlocker
+ *
+ * Copyright (C) 2024  Daniel K. O.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
+#include <wups.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+WUPS_PLUGIN_NAME(PACKAGE_NAME);
+WUPS_PLUGIN_DESCRIPTION("Always enable uploading screenshots from the web browser.");
+WUPS_PLUGIN_VERSION(PACKAGE_VERSION);
+WUPS_PLUGIN_AUTHOR("Daniel K. O.");
+WUPS_PLUGIN_LICENSE("GPLv3");
+
+
+DECL_FUNCTION(BOOL,
+              OSSetScreenCapturePermission,
+              BOOL /*enable*/)
+{
+    return real_OSSetScreenCapturePermission(true);
+}
+
+WUPS_MUST_REPLACE(OSSetScreenCapturePermission,
+                  WUPS_LOADER_LIBRARY_COREINIT,
+                  OSSetScreenCapturePermission);
+
+
+DECL_FUNCTION(BOOL,
+              OSSetScreenCapturePermissionEx,
+              BOOL /*enable_tv*/,
+              BOOL /*enable_drc*/)
+{
+    return real_OSSetScreenCapturePermissionEx(true, true);
+}
+
+WUPS_MUST_REPLACE(OSSetScreenCapturePermissionEx,
+                  WUPS_LOADER_LIBRARY_COREINIT,
+                  OSSetScreenCapturePermissionEx);
